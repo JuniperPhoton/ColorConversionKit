e3867c16983592fd53d0ef90c3ec52881ab0cf66
dengweichao <dengweichao@hotmail.com>
2024-12-11 16:30:49 +0800

    Add docs

---
+++ docs.sh
@@ -0,0 +1,13 @@
+# Run the xcodebuild command
+xcodebuild docbuild -scheme PhotonColorConversionKit \
+    -derivedDataPath ./.build/derived-data \
+    -destination 'generic/platform=iOS' \
+    DOCC_HOSTING_BASE_PATH='PhotonColorConversionKit'
+
+# Check if the command was successful
+if [ $? -eq 0 ]; then
+    # Find and copy the .doccarchive to the current directory, renaming it to 'docs'
+    find ./.build/derived-data -type d -name 'PhotonColorConversionKit.doccarchive' -exec cp -R {} ./docs \;
+else
+    echo "xcodebuild command failed."
+fi
